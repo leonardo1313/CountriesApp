b449187569325f1fa5f9fa3ac13169a8210eccaf
Leonardo_13 <leonardo.novak5@gmail.com>
2023-07-10 11:09:28 +0200

    Initial commit - packages for MVVM architecture created and dependencies added

---
+++ app/src/main/java/com/example/countriesapp/model/Data.kt
@@ -0,0 +1,3 @@
+package com.example.countriesapp.model
+
+data class Country(val countryName: String?)
---
+++ app/src/main/java/com/example/countriesapp/viewmodel/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.countriesapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+
+}
